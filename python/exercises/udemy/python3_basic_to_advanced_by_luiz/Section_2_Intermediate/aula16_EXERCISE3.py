fd163ebb2ee290f86b9a8c31bd9038ee5efb34a7
hey-ditto <hdiasbene@proton.me>
2025-06-04 21:51:04 -0300

    chore(python): added dictionaries exercises

--- python/exercises/udemy/python3_basic_to_advanced_by_luiz/Section_2_Intermediate/aula16_EXERCISE3.py
+++ python/exercises/udemy/python3_basic_to_advanced_by_luiz/Section_2_Intermediate/aula16_EXERCISE3.py
@@ -17,6 +17,28 @@ questions = [
         'Answer': '5',
     },
 ]
+# iterate over all options, to get the keys and values
+counter = 0
+number_questions = 0
+for alternative in questions:
+    number_questions += 1
+    for keys in alternative:
+        if keys == 'Question':
+            print(f'{keys}: {alternative[keys]}\n')
+        if keys == 'Options':
+            print(f'{keys}:')
+            options_list = alternative[keys]
+            for i in range(len(options_list)): # Iterate over the list to display options 
+                print(f'{i}) {options_list[i]}')
+                if options_list[i] == alternative['Answer']:
+                    answer_index = i # Capture the index of the answer for the user input
+        if keys == 'Answer':
+            test_answer = int(alternative[keys])
+            user_answer = int(input("Choose an option: "))
+            if user_answer == answer_index:
+                counter += 1
+                print("You did it!!!\n")
+            else:
+                print("Wrong answer.\n")
 
-# for alternatives in questions:
-#     for keys in alternatives:
+print(f'Congratulations! You got {counter} of {number_questions} questions right!')
